d62ed3fc185e7ddf56a2366f6af6545ec88bcd0e
sophia <ec2-user@ip-172-31-0-202.us-east-2.compute.internal>
2018-06-07 06:25:12 +0000

    post

--- config/app.php
+++ config/app.php
@@ -192,7 +192,7 @@ return [
 
     'aliases' => [
 
-'App'       => Illuminate\Support\Facades\App::class,
+        'App'       => Illuminate\Support\Facades\App::class,
         'Artisan'   => Illuminate\Support\Facades\Artisan::class,
         'Auth'      => Illuminate\Support\Facades\Auth::class,
         'Blade'     => Illuminate\Support\Facades\Blade::class,
