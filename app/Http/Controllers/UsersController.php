49cc6adbe92cdda6b3a5cee8ec49d3369db4fc49
sophia <ec2-user@ip-172-31-0-202.us-east-2.compute.internal>
2018-06-14 07:12:43 +0000

    timeline

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -8,7 +8,7 @@ use App\User; // add
 
 class UsersController extends Controller
 {
-     public function index()
+    public function index()
     {
         $users = User::paginate(10);
         
@@ -17,7 +17,7 @@ class UsersController extends Controller
         ]);
     }
     
-     public function show($id)
+    public function show($id)
     {
         $user = User::find($id);
         $microposts = $user->microposts()->orderBy('created_at', 'desc')->paginate(10);
@@ -30,5 +30,35 @@ class UsersController extends Controller
         $data += $this->counts($user);
 
         return view('users.show', $data);
+    }
+    
+    public function followings($id)
+    {
+        $user = User::find($id);
+        $followings = $user->followings()->paginate(10);
+
+        $data = [
+            'user' => $user,
+            'users' => $followings,
+        ];
+
+        $data += $this->counts($user);
+
+        return view('users.followings', $data);
+    }
+
+    public function followers($id)
+    {
+        $user = User::find($id);
+        $followers = $user->followers()->paginate(10);
+
+        $data = [
+            'user' => $user,
+            'users' => $followers,
+        ];
+
+        $data += $this->counts($user);
+
+        return view('users.followers', $data);
     }
 }
